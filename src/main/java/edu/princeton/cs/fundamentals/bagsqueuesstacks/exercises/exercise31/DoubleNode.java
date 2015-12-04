a3858b65e350e8df34bebe341812896e0792c052
huangjing <hj005zy@gmail.com>
2015-12-04 08:50:06 +0800

    add exercises of chapter 1.3

---
+++ src/main/java/edu/princeton/cs/fundamentals/bagsqueuesstacks/exercises/exercise31/DoubleNode.java
@@ -0,0 +1,17 @@
+package edu.princeton.cs.fundamentals.bagsqueuesstacks.exercises.exercise31;
+
+public class DoubleNode<Item> {
+
+    private Item item;
+
+    private DoubleNode<Item> next;
+
+    private DoubleNode<Item> prev;
+
+    public static <Item> void insert(DoubleNode<Item> node, Item item) {
+        if (node == null) {
+            return;
+        }
+
+    }
+}
